b441e14634f873c8f4ec189f3c88d877ad6970b2
iluxandr0 <inik3004@gmail.com>
2021-05-26 19:36:41 +0500

    add switching between modal windows in qa.html & update buttons and selects

--- src/admin/static/js/modal_load_date/modal_qa.js
+++ src/admin/static/js/modal_load_date/modal_qa.js
@@ -1,13 +1,14 @@
 $(function() {
 	$(".view_icon").click(
 		function() {
+			var id = $(this).parents('.table_line_item').attr('data-id')
 			var category = $(this).parents('.table_line_item').attr('data-category');
 			var popular = $(this).parents('.table_line_item').attr('data-popular');
 			var question = $(this).parents('.table_line_item').attr('data-question');
 			var answer = $(this).parents('.table_line_item').attr('data-answer');
+			var cat_id = $(this).parents('.table_line_item').attr('data-cat_id');
 
 			$("#view_category_content").html(category);
-			var star = ""
 			if (popular == "True") {
 				star = '<span class="fa-stack"><i class="fa fa-star fa-stack-1x fa-lg"></i><i class="fa fa-star-o fa-stack-1x fa-lg"></i></span>';
 			} else {
@@ -16,6 +17,13 @@ $(function() {
 			$("#view_popular_content").html(star);
 			$("#view_question_content").html(question);
 			$("#view_answer_content").html(answer);
+
+
+			$("#viewModal").attr("data-id", id);
+			$("#viewModal").attr("data-cat_id", cat_id);
+			$("#viewModal").attr("data-popular", popular);
+			$("#viewModal").attr("data-question", question);
+			$("#viewModal").attr("data-answer", answer);
 		})
 });
 
@@ -53,6 +61,46 @@ $(function() {
 			$("#delete_question_content").html(question);
 		})
 });
+
+$(function() {
+	$(".btn_view_edit").click(
+		function() {
+			var id = $("#viewModal").attr('data-id')
+			var question = $("#viewModal").attr('data-question');
+			var answer = $("#viewModal").attr('data-answer');
+			var is_popular = $("#viewModal").attr('data-popular');
+			var cat_id = $("#viewModal").attr('data-cat_id');
+
+			$("#edit_id").val(id);
+			$("#edit_question_content").val(question);
+			$('#edit_answer_content').summernote('insertText', '');
+			$('#edit_answer_content').summernote('code', '');
+			
+			$('#edit_answer_content').summernote('pasteHTML', answer);
+			if (is_popular == "True") {
+				$("#edit_popular_content").attr("checked","checked");
+			} else {
+				$("#edit_popular_content").removeAttr("checked");
+			}
+			$("#edit_cat_id_content").val(cat_id);
+		})
+});
+
+$(function() {
+	$(".btn_view_delete").click(
+		function() {
+			var id = $("#viewModal").attr('data-id')
+			var question = $("#viewModal").attr('data-question');
+
+			$("#delete_id").val(id);
+			$("#delete_question_content").html(question);
+		})
+});
+
+
+
+
+
 
 
 
