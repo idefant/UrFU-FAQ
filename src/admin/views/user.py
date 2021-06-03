afbd46303e830f98a26de7353efbd9a91ad821d7
iluxandr0 <inik3004@gmail.com>
2021-06-03 11:39:15 +0500

    move color view to ViewCategory

--- src/admin/views/user.py
+++ src/admin/views/user.py
@@ -1,11 +1,8 @@
-from flask import render_template, request, flash, url_for
+from flask import render_template
 from flask_classy import FlaskView, route
 from flask_login import login_required, current_user
-from sqlalchemy import desc
-from werkzeug.utils import redirect
-
 import forms
-from models import Categories, Questions, Users
+from models import Users
 
 
 class ViewUser(FlaskView):
@@ -55,61 +52,3 @@ class ViewUser(FlaskView):
         except NameError:
             return render_template("admin/error_page.html", message="Ошибка чтения из БД")
         return render_template('admin/users_rights.html', users=users, edit_user_rights_form=edit_user_rights_form)
-
-    @route('/colors')
-    @login_required
-    def colors(self):
-        if not current_user.right_category:
-            return render_template('admin/access_denied.html')
-        cat_id_data = request.args.get("cat_id")
-
-        try:
-            questions = Questions.query
-            categories = Categories.query.order_by(desc(Categories.priority))
-        except (NameError, AttributeError):
-            return render_template("admin/error_page.html", message="Ошибка чтения из БД")
-
-        if cat_id_data is None:
-            flash('Категория не выбрана', category='danger')
-            return redirect(url_for('.ViewCategory:category'))
-        elif cat_id_data == "popular":
-            is_popular_category = True
-
-            current_category = categories.get(0)
-            if current_category is None:
-                flash('Нет такой категории в БД', category='danger')
-                return redirect(url_for('.ViewCategory:category'))
-            category_index = categories.all().index(current_category)
-            try:
-                popular_qa_list = questions.filter(Questions.is_popular).order_by(Questions.popular_priority)
-            except (NameError, AttributeError):
-                return render_template("admin/error_page.html", message="Ошибка чтения из БД")
-
-            qa_list = []
-            for qa in popular_qa_list:
-                category = categories.get(qa.cat_id)
-                if category is not None:
-                    qa_list += [(qa, category.icon_name)]
-
-        else:
-            is_popular_category = False
-            if not cat_id_data.isdigit():
-                flash('ID категории должен быть числом', category='danger')
-                return redirect(url_for('.ViewCategory:category'))
-            else:
-                cat_id = int(cat_id_data)
-            current_category = categories.get(cat_id)
-            if current_category is None or cat_id == 0:
-                flash('Нет такой категории в БД', category='danger')
-                return redirect(url_for('.ViewCategory:category'))
-
-            category_index = categories.all().index(current_category)
-
-            try:
-                qa_list = questions.filter(Questions.cat_id == cat_id).order_by(Questions.priority)
-            except (NameError, AttributeError):
-                return render_template("admin/error_page.html", message="Ошибка чтения из БД")
-
-        return render_template('admin/color_picker.html', qa_list=qa_list, categories_list=categories,
-                               current_category=current_category, index=category_index,
-                               is_popular_category=is_popular_category)
